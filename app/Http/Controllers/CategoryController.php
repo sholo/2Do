84f805e1ccd08a61dd1ad77bb403d0d4e19065b6
sholo <juancfg_18@hotmail.com>
2018-03-09 17:19:52 -0800

    Created the functionality for all the verbs

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -23,16 +23,6 @@ class CategoryController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -59,37 +49,35 @@ class CategoryController extends Controller
         return response()->json($resource);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param CategoryTransformer $transformer
+	 * @param  \Illuminate\Http\Request $request
+	 * @param  int $id
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+    public function update(CategoryTransformer $transformer, Request $request, $id)
     {
-        //
+	    $category = Category::find($id);
+	    $category->fill($request->all())->save();
+	    $resource = $transformer->item($category);
+	    return response()->json($resource);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int $id
+	 *
+	 * @return \Illuminate\Http\Response
+	 * @throws \Exception
+	 */
     public function destroy($id)
     {
-        //
+	    $category = Category::find($id);
+	    $category->delete();
+	    return response()->json(['message' => 'category_deleted'], 201);
     }
 }
